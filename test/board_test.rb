e7f05bf06cafb167f2ec8682803166022192624f
Phil DeLong <philjdelong@gmail.com>
2019-10-10 13:45:37 -0600

    Finish validation test draft, need to review

--- test/board_test.rb
+++ test/board_test.rb
@@ -45,10 +45,24 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A22")
   end
 
-  def test_it_can_validate_placement
+  def test_it_can_validate_placement_by_length
     # skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2",])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_it_can_validate_placement_by_consecutive
+    # skip
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["C1", "B1"])
+  end
+
+  def test_it_can_validate_placement_by_diagonal
+    # skip
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["C2", "D3"])
   end
 
 end
