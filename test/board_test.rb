28ac1eb92110b298dd6d50f9c89f89893b8a3225
Phil DeLong <philjdelong@gmail.com>
2019-10-10 12:39:35 -0600

    Pass exists, valid_coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,48 +1,42 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '.lib/ship'
-require '.lib/cell'
-require '.lib/board'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
 require 'pry'
 
 class BoardTest < Minitest::Test
 
   def setup
-    @cells = Cell.new{
-      'A1',
-      'A2',
-      'A3',
-      'A4',
-      'B1',
-      'B2',
-      'B3',
-      'B4',
-      'C1',
-      'C2',
-      'C3',
-      'C4',
-      'D1',
-      'D2',
-      'D3',
-      'D4'
+    cells = {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4"),
     }
-    @board = Board.new()
-
+      @board = Board.new(cells)
   end
 
   def test_it_exists
-    skip
-    assert_instance_of Board, board
+    # skip
+    assert_instance_of Board, @board
   end
 
-  def test_
-    skip
-    assert_equal
-  end
-
-  def test_
-    skip
-    assert_equal
+  def test_it_has_a_valid_coordinate
+    # skip
+    assert_equal true, @board.valid_coordinate?("A1")
   end
 
 end
