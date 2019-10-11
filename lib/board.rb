f910114a0d494ce3357f3c00860edcbd4b36b715
Phil DeLong <philjdelong@gmail.com>
2019-10-11 11:44:35 -0600

    Add helper methods, vertical/horizontal'

--- lib/board.rb
+++ lib/board.rb
@@ -11,20 +11,26 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    length && (vertical || horizontal)
+    # vertical = (coordinates[0].split('')[0] == coordinates[1].split('')[0])
+    # horizontal = (coordinates[0].split('')[1] == coordinates[1].split('')[1])
     #length validation
     length = ship.length == coordinates.count
-    vertical = [coordinates[0].split('')[0] == coordinates[1].split('')[0]]
-    horizontal = [coordinates[0].split('')[1] == coordinates[1].split('')[1]]
-
     #consecutive validation
-    # if coordinates[0].split('')[0] == coordinates[1].split('')[0]
-    #   true
-    #   else if coordinates[0].split('')[1] == coordinates[1].split('')[1]
-    #     true
-    #   else
-    #     false
-    # end
+    length && (vertical || horizontal)
+    # binding.pry
+
+    # coordinates.each do |coordinate|
+    #   if coordinate.split('')[0].succ ==
+  end
+
+  def vertical
+    (coordinates[0].split('')[0] == coordinates[1].split('')[0])
+    # coordinates.each do |coordinate|
+        coordinate.split('')[0].succ
+  end
+
+  def horizontal
+    horizontal = (coordinates[0].split('')[1] == coordinates[1].split('')[1])
   end
 
 end
